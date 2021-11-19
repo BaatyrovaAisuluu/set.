13061cb099de4c1c4018a43c2fee68e014b42115
User <trrioiw>
2021-11-19 13:44:29 +0600

    test

---
+++ src/com/company/Main.java
@@ -0,0 +1,76 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Set<String>students=new HashSet<>();//аралаш чыгарат
+//        students.add("марат");
+//        students.add("кубат");
+//        students.add("муха");
+//        students.add("рахим");
+//        System.out.println(students);
+//
+//
+//
+//        Set<String>students1=new TreeSet<>();//алфабит менен чыгарат
+//        students1.add("марат");
+//        students1.add("кубат");
+//        students1.add("муха");
+//        students1.add("рахим");
+//        System.out.println(students1);
+//
+//
+//
+//
+//        Set<String>students2=new LinkedHashSet<>();//сорттойт кандай жазсак ошондой чыгырат
+//        students2.add("марат");
+//        students2.add("кубат");
+//        students2.add("муха");
+//        students2.add("рахим");
+//        System.out.println(students2);
+
+
+//        Set<Integer> integers=new TreeSet<>
+//                (Arrays.asList(3,5,4,2,6,8,7,54,24,5,16,17,45,3,4));
+//        System.out.println(integers);
+
+
+
+
+//Set<Integer> integers=new HashSet<>();
+//Random random=new Random();
+//int sum=0;
+//        for (int i = 0; i <100; i++) {
+//            int a= random.nextInt(400);
+//            integers.add(a);
+//
+//            sum+=a;
+//        }
+//        System.out.println(integers);
+//        System.out.println(sum);
+//
+
+
+String name= "самат";
+String s="fss";
+        System.out.println(s.hashCode());
+        System.out.println(name.hashCode());//бул сан
+
+        String name1="марат";
+        String name2="кара";
+        boolean sum1=name1.equals(name2);
+        System.out.println(sum1);
+
+        System.out.println();
+
+
+
+
+
+
+    }
+    }
+
